c16675b55d53e9ea2e6ca311791ea384c1eb038a
yuichi-tanaka <A12473@PC-7179.local>
2014-05-23 22:43:28 +0900

    a

--- libs/core/compress.js
+++ libs/core/compress.js
@@ -4,14 +4,24 @@ var debug = require('../debug').log;
 
 var compress = module.exports = function(options){
   Transform.call(this,options);
+  this._buf = '';
 };
 util.inherits(compress,Transform);
 
 compress.prototype._transform = function(chunk,enc,fn){
   var self = this;
+
+  var rows = chunk.toString().split('\n');
+  for(var i = 0; i < rows.length; i++){
+    _parse_row(rows[i],'sample_tag',',');
+  }
   self.push(chunk);
-  debug('compress','complress');
+  //debug('compress','complress');
   fn();
 };
-//compress.prototype._flush = function(){};
+compress.prototype._flush = function(fn){
+  debug('compress','flush!');
+};
+var _parse_row = function(row,tags,sept){
 
+};
